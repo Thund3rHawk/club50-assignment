2c1b6aaad5d07835d7847441a7a58a8382c601ed
thund3rhawk <2025.it18.swadhinpaul@gmail.com>
2025-04-21 01:39:44 +0530

    chore: add superbse config

---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+
+
+import { View, Text } from 'react-native'
+import { Session } from '@supabase/supabase-js'
+import { supabase } from '@/lib/superbase'
+import Auth from '@/components/Auth'
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+  console.log(session);
+  
+  return (
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
+    </View>
+  )
+}
