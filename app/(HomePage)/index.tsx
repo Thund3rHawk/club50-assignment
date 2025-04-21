415b23cc6a693c7cbe050e24740e31f662e1f48c
thund3rhawk <2025.it18.swadhinpaul@gmail.com>
2025-04-21 12:25:37 +0530

    chore: add nativewind config

--- app/(HomePage)/index.tsx
+++ app/(HomePage)/index.tsx
@@ -1,25 +1,27 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { supabase } from '@/lib/superbase'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { supabase } from "@/lib/superbase";
 
 const HomePage = () => {
-
-    async function signOut(){
-        try {
-            await supabase.auth.signOut();
-        } catch (error) {
-            throw error;
-        }
+  async function signOut() {
+    try {
+      await supabase.auth.signOut();
+    } catch (error) {
+      throw error;
     }
+  }
 
   return (
     <View>
       <Text>Hello from HomePage</Text>
-      <TouchableOpacity onPress={()=>signOut()}>
-        <Text>Sign Out</Text>
+      <TouchableOpacity
+        onPress={() => signOut()}
+        className="w-80 bg-black m-10"
+      >
+        <Text className="text-white">Sign Out</Text>
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,33 @@
-import 'react-native-url-polyfill/auto'
-import { useState, useEffect } from 'react'
+import "react-native-url-polyfill/auto";
+import { useState, useEffect } from "react";
 
-
-import { View, Text } from 'react-native'
-import { Session } from '@supabase/supabase-js'
-import { supabase } from '@/lib/superbase'
-import Auth from '@/components/Auth'
-import HomePage from './(HomePage)'
+import { View, Text } from "react-native";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "@/lib/superbase";
+import Auth from "@/components/Auth";
+import HomePage from "./(HomePage)";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
   console.log(session);
-  
+
   if (!session) {
-    return (
-      <Auth/>
-    )
+    return <Auth />;
   }
 
   return (
     <>
-      <HomePage/>
+      <HomePage />
     </>
-  )
+  );
 }
